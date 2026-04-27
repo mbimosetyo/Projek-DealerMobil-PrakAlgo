fc115fc9eaa900ae72458d564d9b0de1f3f13fd9
Krisnan2 <krisnan364@gmail.com>
2026-04-27 20:09:00 +0700

    Perancangan Struktur Data

--- dealermobil.cpp
+++ dealermobil.cpp
@@ -1,5 +1,84 @@
 #include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <cstring>
 using namespace std;
+// ============================================================
+//  KONSTANTA
+// ============================================================
+#define MAX_STR     100   // Panjang maksimal string/teks
+#define MAX_LOGIN     3   // Maksimal percobaan login
+
+// ============================================================
+//  STRUCT DATA & LINKED LIST NODE
+// ============================================================
+
+struct Mobil {
+    char id[MAX_STR];     
+    char nama[MAX_STR];   
+    char tipe[MAX_STR];   
+    char warna[MAX_STR];  
+    int  tahun;           
+    double harga;         
+    int  stok;            
+};
+
+struct Akun {
+    char username[MAX_STR];     
+    char password[MAX_STR];     
+    char nama_lengkap[MAX_STR]; 
+};
+
+// Node untuk Linked List Mobil
+struct NodeMobil {
+    Mobil data;
+    NodeMobil* next;
+};
+
+// Node untuk Linked List Akun
+struct NodeAkun {
+    Akun data;
+    NodeAkun* next;
+};
+
+// ============================================================
+//  DATA GLOBAL - LINKED LIST POINTERS
+// ============================================================
+NodeMobil* headMobil = NULL;
+int jumlahMobil = 0;
+
+NodeAkun* headAkun = NULL;
+int jumlahAkun = 0;
+
+// ============================================================
+//  FUNGSI PEMBANTU LINKED LIST
+// ============================================================
+
+void insertLastMobil(NodeMobil*& head, Mobil m) {
+    NodeMobil* newNode = new NodeMobil{m, NULL};
+    if (!head) {
+        head = newNode;
+        return;
+    }
+    NodeMobil* curr = head;
+    while (curr->next) {
+        curr = curr->next;
+    }
+    curr->next = newNode;
+}
+
+void insertLastAkun(NodeAkun*& head, Akun a) {
+    NodeAkun* newNode = new NodeAkun{a, NULL};
+    if (!head) {
+        head = newNode;
+        return;
+    }
+    NodeAkun* curr = head;
+    while (curr->next) {
+        curr = curr->next;
+    }
+    curr->next = newNode;
+}
 
 int main(){
 
